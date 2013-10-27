9db2702d116c5837b2f5e97813593b402a19b461
Savpek <savolainen.pekka@gmail.com>
2013-10-27 22:24:47 +0200

    Fixed construction root with new features.

--- WpfDemo/MainWindow.xaml.cs
+++ WpfDemo/MainWindow.xaml.cs
@@ -1,6 +1,10 @@
-﻿using System.Windows;
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Media;
+using SqlScriptRunner.GUI;
 using WpfDemo.Domain;
 using WpfDemo.ViewModel;
+using WpfDemo.ViewModel.Filters;
 
 namespace WpfDemo
 {
@@ -17,8 +21,23 @@ namespace WpfDemo
             // In real world app there should be good framework for this and this should be done
             // outside of main window for reason that then mainwindow is interchangeable too if required.
             var dataSource = new InMemoryRepository();
-            var reportViewModel = new ReportViewModel(dataSource, null);
-            DataContext = reportViewModel;
+
+            // Dummy data for testing...
+            dataSource.Add(new CarImage {Color="Black", RegisterPlate = "ABC-123", Speed = 140});
+
+            var messenger = new MessageAggregator();
+            var reportViewModel = new ReportViewModel(dataSource, messenger);
+            var colorFilter = new ColorFilterViewModel();
+            var filterViewModel = new FiltersViewModel(new List<IFilter> {colorFilter}, messenger);
+            
+            // Just for testing, apply dummy filter so that data is shown.
+            messenger.Publish(new Messages.FiltersAppliedMessage());
+
+            DataContext = new
+            {
+                Report = reportViewModel,
+                Filters = filterViewModel
+            };
         }
     }
 }
