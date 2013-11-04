97b1910dd6de0077a9b2df07ec286baf278f3c1f
Savpek <savolainen.pekka@gmail.com>
2013-11-04 23:19:26 +0200

    Now object properties are formatted with extension method.

--- WpfDemo.Tests/Unit/LoggerHelperUnitTests.cs
+++ WpfDemo.Tests/Unit/LoggerHelperUnitTests.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Diagnostics;
-using System.IO;
-using System.Security.Cryptography.X509Certificates;
 using FluentAssertions;
 using NUnit.Framework;
 using WpfDemo.Components;
@@ -11,54 +8,69 @@ namespace WpfDemo.Tests.Unit
     [TestFixture]
     public class LoggerHelperUnitTests
     {
-        [SetUp]
-        public void Init()
-        {
-        }
-
         private class Dummy
         {
             public string StringProperty { get; set; }
             public int IntProperty { get; set; }
             public object ObjectProperty { get; set; }
 
+            public object NonPropertyMethod()
+            {
+                return null;
+            }
+
             public override string ToString()
             {
-                return string.Format("'StringProperty':'{0}', 'IntProperty':'{1}', 'ObjectProperty':'{2}'", StringProperty, IntProperty, ObjectProperty);
+                return string.Format("Dummy: 'StringProperty':'{0}', 'IntProperty':'{1}', 'ObjectProperty':'{2}'", StringProperty, IntProperty, ObjectProperty);
             }
         }
 
         [Test]
         public void WithDefinedProperties_PrintValuesAndNamesCorrectly()
         {
-
             var dummy = new Dummy {StringProperty = "StringProperty value", IntProperty = 10};
-            var results = LoggerHelper.PropertyPrint(dummy, x => x.StringProperty, x => x.IntProperty);
+            var results = dummy.FormatProperties(x => x.StringProperty, x => x.IntProperty);
 
-            results.Should().Be("{'StringProperty':'StringProperty value', 'IntProperty':'10', }");
+            results.Should().Be("{Dummy: 'StringProperty':'StringProperty value', 'IntProperty':'10', }");
         }
 
         [Test]
-        public void WithNullValues_PrintFieldsAsEmpty()
+        public void WithNullValues_PrintFieldsAsNull()
         {
             var dummy = new Dummy();
-            var results = LoggerHelper.PropertyPrint(dummy, x => x.ObjectProperty);
+            var results = dummy.FormatProperties(x => x.ObjectProperty);
 
-            results.Should().Be("{'ObjectProperty':'', }");
+            results.Should().Be("{Dummy: 'ObjectProperty':'null', }");
         }
 
         [Test]
-        public void WithVeryLargeAmountOfPrints_BeNearlyAsFastAsToCommonManualToString()
+        public void WithZeroProperties_ThrowNewInvalidOperationException()
         {
-            this.Invoking(x => x.TestExpressionVersion(new Dummy(), 100000)).ExecutionTime().ShouldNotExceed(100.Milliseconds());
-            this.Invoking(x => x.TestToStringVersion(new Dummy(), 100000)).ExecutionTime().ShouldNotExceed(100.Milliseconds());
+            var dummy = new Dummy();
+            dummy.Invoking(d => d.FormatProperties())
+                .ShouldThrow<InvalidOperationException>();
+        }
+
+        [Test]
+        public void InvokeNonPropertyMethod_ThrowInvalidOperationException()
+        {
+            var dummy = new Dummy();
+            dummy.Invoking(d => d.FormatProperties(x => x.NonPropertyMethod()))
+                .ShouldThrow<InvalidOperationException>();
+        }
+
+        [Test]
+        public void WithVeryLargeAmountOfPrints_BeFastEnough()
+        {
+            this.Invoking(x => x.TestExpressionVersion(new Dummy(), 10000)).ExecutionTime().ShouldNotExceed(300.Milliseconds());
+            this.Invoking(x => x.TestToStringVersion(new Dummy(), 10000)).ExecutionTime().ShouldNotExceed(10.Milliseconds());
         }
 
         private void TestExpressionVersion(Dummy targetObject, int count)
         {
             for (int i = 0; i < count; i++)
             {
-                var devNull = LoggerHelper.PropertyPrint(targetObject, 
+                var devNull = FormatExtensions.FormatProperties(targetObject, 
                     x => x.IntProperty, 
                     x => x.ObjectProperty,
                     x => x.StringProperty);
---
+++ WpfDemo/Components/FormatExtensions.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace WpfDemo.Components
+{
+    public static class FormatExtensions
+    {
+        // Reflection is slow, this cache gives ~200 times faster execution times.
+        private static readonly ConcurrentDictionary<string, object> ExpressionCache = new ConcurrentDictionary<string, object>();
+
+        public static string FormatProperties<T>(this T targetObject, params Expression<Func<T, object>>[] expressions)
+        {
+            if(expressions.Length == 0)
+                throw new InvalidOperationException("Tried to print class with properties formatting, but no properties defined!");
+
+            var results = "{" + typeof(T).Name + ": ";
+            const string template = "'{0}':'{1}', ";
+
+            foreach (var expression in expressions)
+            {
+                results += string.Format(template, GetExpressionName(expression), GetExpressionValue(expression, targetObject));
+            }
+
+            return results + "}";
+        }
+
+        private static object GetExpressionValue<T>(Expression<Func<T, object>> expression, T targetObject)
+        {
+            var key = typeof(T).Name + expression;
+
+            if (ExpressionCache.ContainsKey(key))
+                return ((Func<T, object>)ExpressionCache[key]).Invoke(targetObject);
+
+            var compiled = expression.Compile();
+            ExpressionCache.TryAdd(key, compiled);
+
+            return compiled.Invoke(targetObject) ?? "null";
+        }
+
+        private static string GetExpressionName<T>(Expression<Func<T, object>> expression)
+        {
+            var memberBody = expression.Body as MemberExpression;
+            if (memberBody != null)
+                return memberBody.Member.Name;
+
+            var unaryBody = expression.Body as UnaryExpression;
+            if (unaryBody != null)
+            {
+                var operand = unaryBody.Operand;
+                return ((MemberExpression)operand).Member.Name;    
+            }
+
+            throw new InvalidOperationException(
+                string.Format("Tried to format property from object, but defined field is not property. Invalid expression: '{0}'", expression));
+        }
+    }
+}
